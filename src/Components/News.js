20673e49ca12cc268fa08d31dc439474a4ee75cd
Nikhil Choudhary <nickchoudharyc72@gmail.com>
2023-08-01 19:08:55 +0530

    API Fetched (L-24)

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-  render() {
-    return (
-      <div>
-        This is news
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-        <NewsItem/>
-      </div>
-    )
-  }
+    render() {
+        return (
+            <div className='container my-3'>
+                <h2>News Monkey - Top Headlines</h2>
+                <div className="row">
+                    <div className="col-md-4">
+                        <NewsItem title='mytile' description="descriptopn" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title='mytile' description="descriptopn" />
+                    </div>
+                    <div className="col-md-4">
+                        <NewsItem title='mytile' description="descriptopn" />
+                    </div>
+                </div>
+               
+            </div>
+        )
+    }
 }
 
 export default News
