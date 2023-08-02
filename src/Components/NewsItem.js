bae2da91af4a55e602ae1d128d799558a1280ae2
Nikhil Choudhary <nickchoudharyc72@gmail.com>
2023-08-02 14:01:47 +0530

    Using Fetch API in React LS_27

--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -6,7 +6,7 @@ export class NewsItem extends Component {
         return (
             <div className='my-3'>
                 <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl} className="card-img-top" alt="..." />
+                    <img src={imageUrl?imageUrl:"https://img.freepik.com/free-photo/digital-world-map-hologram-blue-background_1379-901.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
