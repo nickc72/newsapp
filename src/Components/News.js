669509d85804f86f3edc629c6d56a952cc5a855e
Nikhil Choudhary <nickchoudharyc72@gmail.com>
2023-08-02 14:56:05 +0530

    Adding Previous & Next Buttons LS_28

--- src/Components/News.js
+++ src/Components/News.js
@@ -2,22 +2,52 @@ import React, { Component } from 'react'
 import NewsItem from './NewsItem'
 
 export class News extends Component {
-    
+
     constructor() {
         super();
         this.state = {
             articles: [],
-            loading: false
+            loading: false,
+            page: 1
         }
     }
 
-    async componentDidMount(){
-        let url="https://newsapi.org/v2/top-headlines?country=in&apiKey=c22e7e845f764216816f49f56c6c3283";
+    async componentDidMount() {
+        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=c22e7e845f764216816f49f56c6c3283&pageSize=20";
         let data = await fetch(url)
         let parsedData = await data.json()
         console.log(parsedData)
-        this.setState({articles:parsedData.articles})
+        this.setState({ articles: parsedData.articles, totalResults: parsedData.totalResults })
     }
+
+    handleNextClick = async () => {
+        if (this.state.page + 1 > Math.ceil(this.state.totalResults / 20)) {
+
+        }
+        else {
+
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c22e7e845f764216816f49f56c6c3283&page=${this.state.page + 1}&pageSize=20`;
+            let data = await fetch(url)
+            let parsedData = await data.json()
+            console.log(parsedData)
+            this.setState({
+                page: this.state.page + 1,
+                articles: parsedData.articles
+            })
+        }
+    }
+
+    handlePrevClick = async () => {
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=c22e7e845f764216816f49f56c6c3283&page=${this.state.page - 1}&pageSize=20`;
+        let data = await fetch(url)
+        let parsedData = await data.json()
+        console.log(parsedData)
+        this.setState({
+            page: this.state.page - 1,
+            articles: parsedData.articles
+        })
+    }
+
     render() {
         return (
             <div className='container my-3'>
@@ -25,13 +55,16 @@ export class News extends Component {
                 <div className="row">
                     {this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
-                            <NewsItem title={element.title?element.title.slice(0,45):""} description={element.description?element.description.slice(0,88):""} imageUrl={element.urlToImage} newsUrl={element.url} />
+                            <NewsItem title={element.title ? element.title.slice(0, 45) : ""} description={element.description ? element.description.slice(0, 88) : ""} imageUrl={element.urlToImage} newsUrl={element.url} />
                         </div>
                     })}
+                </div>
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page <= 1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}>&larr; Previous</button>
+                    <button type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
                 </div>
             </div>
         )
     }
 }
-
 export default News
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -2,15 +2,15 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description, imageUrl, newsUrl} = this.props
+        let { title, description, imageUrl, newsUrl } = this.props
         return (
             <div className='my-3'>
-                <div className="card" style={{width: "18rem"}}>
-                    <img src={imageUrl?imageUrl:"https://img.freepik.com/free-photo/digital-world-map-hologram-blue-background_1379-901.jpg"} className="card-img-top" alt="..." />
+                <div className="card" style={{ width: "18rem" }}>
+                    <img src={imageUrl ? imageUrl : "https://img.freepik.com/free-photo/digital-world-map-hologram-blue-background_1379-901.jpg"} className="card-img-top" alt="..." />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
                         <p className="card-text">{description}...</p>
-                        <a href={newsUrl} target='_blank' className="btn btn-sm btn-primary">Read More</a>
+                        <a rel="noreferrer" href={newsUrl} target='_blank' className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
             </div>
